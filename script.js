6919b090a13c1c3e1b9fbee3f867e5aa90f4e6e9
Shravya Vaka <shravyavaka04@gmail.com>
2026-08-20 21:08:12 +0530

    create registration page

---
+++ script.js
@@ -0,0 +1,24 @@
+let form = document.getElementById("registrationForm");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    let message = document.getElementById("message");
+
+    if (name == "" || email == "" || password == "" || confirmPassword == "") {
+        message.innerHTML = "Please fill all the fields.";
+        return;
+    }
+
+    if (password != confirmPassword) {
+        message.innerHTML = "Passwords do not match.";
+        return;
+    }
+
+    message.innerHTML = "Registration successful!";
+});
